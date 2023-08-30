9aa6be9fd93c41e4b6137f0eb68eab7e57aaedbe
kyeong-hyeok <minkh4227@naver.com>
2023-08-30 15:20:54 +0900

    feat: 프로젝트 생성 (#1)

---
+++ src/main/java/com/teamA/hicardi/HicardiApplication.java
@@ -0,0 +1,13 @@
+package com.teamA.hicardi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HicardiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HicardiApplication.class, args);
+	}
+
+}
