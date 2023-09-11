9fcdccdca77c37e121c304681634b572521b0341
kyeong-hyeok <minkh4227@naver.com>
2023-09-11 12:41:38 +0900

    feat: FAQ 전체 조회 기능 구현 (#38)

--- src/main/java/com/teamA/hicardi/domain/faq/repository/FaqRepository.java
+++ src/main/java/com/teamA/hicardi/domain/faq/repository/FaqRepository.java
@@ -1,7 +1,11 @@
 package com.teamA.hicardi.domain.faq.repository;
 
 import com.teamA.hicardi.domain.faq.entity.Faq;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FaqRepository extends JpaRepository<Faq, Long> {
+
+    Page<Faq> findAll(Pageable pageable);
 }
