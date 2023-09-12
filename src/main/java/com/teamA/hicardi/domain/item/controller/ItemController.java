0a8a205d2414d647eef8625419b4f39e2dd01907
kimdohyung <pdohyung@gmail.com>
2023-09-12 23:59:47 +0900

    feat: 상품 상세 조회 기능 구현

--- src/main/java/com/teamA/hicardi/domain/item/controller/ItemController.java
+++ src/main/java/com/teamA/hicardi/domain/item/controller/ItemController.java
@@ -2,6 +2,7 @@ package com.teamA.hicardi.domain.item.controller;
 
 import com.teamA.hicardi.common.dto.PageResponseDto;
 import com.teamA.hicardi.domain.item.dto.response.ItemGetAllResponseDto;
+import com.teamA.hicardi.domain.item.dto.response.ItemGetResponseDto;
 import com.teamA.hicardi.domain.item.service.ItemService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,16 @@ public class ItemController {
     public ResponseEntity<PageResponseDto> getAllItems(Pageable pageable) {
         Page<ItemGetAllResponseDto> response = itemService.getAllItems(pageable);
         return PageResponseDto.of(response);
+    }
+
+    @Operation(summary = "상품 상세 조회", description = "상품을 조회합니다.",
+        security = { @SecurityRequirement(name = "bearer-key") },
+        responses = {
+            @ApiResponse(responseCode = "200", description = "상품 상세 조회 성공")
+        })
+    @GetMapping("/{itemId}")
+    public ResponseEntity<ItemGetResponseDto> getItem(@PathVariable Long itemId){
+        ItemGetResponseDto itemGetResponseDto = itemService.getItem(itemId);
+        return ResponseEntity.ok(itemGetResponseDto);
     }
 }
