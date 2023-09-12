720ba36992bb3f41b0ddd0d0516bc632bf5567bb
kyeong-hyeok <minkh4227@naver.com>
2023-09-12 12:03:21 +0900

    refactor: Category description 추가 및 FAQ 조회 Dto 수정 (#43)

--- src/main/java/com/teamA/hicardi/domain/faq/dto/response/FaqGetResponseDto.java
+++ src/main/java/com/teamA/hicardi/domain/faq/dto/response/FaqGetResponseDto.java
@@ -3,9 +3,9 @@ package com.teamA.hicardi.domain.faq.dto.response;
 import com.teamA.hicardi.domain.faq.entity.Category;
 import com.teamA.hicardi.domain.faq.entity.Faq;
 
-public record FaqGetResponseDto(Long id, Category category, String question, String answer) {
+public record FaqGetResponseDto(Long id, String category, String question, String answer) {
 
     public static FaqGetResponseDto from(Faq faq) {
-        return new FaqGetResponseDto(faq.getId(), faq.getCategory(), faq.getQuestion(), faq.getAnswer());
+        return new FaqGetResponseDto(faq.getId(), faq.getCategory().getDesc(), faq.getQuestion(), faq.getAnswer());
     }
 }
--- src/main/java/com/teamA/hicardi/domain/faq/entity/Category.java
+++ src/main/java/com/teamA/hicardi/domain/faq/entity/Category.java
@@ -3,11 +3,15 @@ package com.teamA.hicardi.domain.faq.entity;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.teamA.hicardi.error.ErrorCode;
 import com.teamA.hicardi.error.exception.custom.BusinessException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.stream.Stream;
 
+@Getter
+@AllArgsConstructor
 public enum Category {
-    USE, DELIVERY, DEVICE, ETC;
+    USE("사용법"), DELIVERY("배송"), DEVICE("기기"), ETC("기타");
 
     @JsonCreator
     public static Category create(String requestValue) {
@@ -16,4 +20,6 @@ public enum Category {
                 .findFirst()
                 .orElseThrow(() -> new BusinessException(ErrorCode.INVALID_CATEGORY));
     }
+
+    private final String desc;
 }
