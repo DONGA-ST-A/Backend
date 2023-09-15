2a46384b642196b3aa72e21d0e4e72c77fe3c8b4
kimdohyung <pdohyung@gmail.com>
2023-09-15 22:01:39 +0900

    feat: NoticeCategory를 파라미터로 받기 위한 코드 추가

--- src/main/java/com/teamA/hicardi/domain/notice/entity/NoticeCategory.java
+++ src/main/java/com/teamA/hicardi/domain/notice/entity/NoticeCategory.java
@@ -1,5 +1,12 @@
 package com.teamA.hicardi.domain.notice.entity;
 
+import java.util.stream.Stream;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.teamA.hicardi.domain.faq.entity.Category;
+import com.teamA.hicardi.error.ErrorCode;
+import com.teamA.hicardi.error.exception.custom.BusinessException;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,6 +14,14 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum NoticeCategory {
 	NECESSARY("필독"), UPDATE("업데이트"), DATA("자료"), NEWS("언론 보도");
+
+	@JsonCreator
+	public static NoticeCategory create(String requestValue) {
+		return Stream.of(values())
+			.filter(v -> v.desc.equalsIgnoreCase(requestValue))
+			.findFirst()
+			.orElseThrow(() -> new BusinessException(ErrorCode.INVALID_CATEGORY));
+	}
 
 	private final String desc;
 }
