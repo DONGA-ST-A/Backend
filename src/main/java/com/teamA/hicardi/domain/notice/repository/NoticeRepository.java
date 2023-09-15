3a645c2b8a377ccb94331d1eef7e27c60a2d5d3f
kimdohyung <pdohyung@gmail.com>
2023-09-15 22:04:17 +0900

    feat: NoticeCategory로 조회하기 위한 메서드 추가

--- src/main/java/com/teamA/hicardi/domain/notice/repository/NoticeRepository.java
+++ src/main/java/com/teamA/hicardi/domain/notice/repository/NoticeRepository.java
@@ -5,8 +5,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import com.teamA.hicardi.domain.notice.entity.Notice;
+import com.teamA.hicardi.domain.notice.entity.NoticeCategory;
+import io.lettuce.core.dynamic.annotation.Param;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
+	@Query("SELECT n FROM Notice n " +
+		"WHERE (n.isTop = true OR n.category = :category) " +
+		"ORDER BY n.isTop DESC, n.createdDate DESC")
+	Page<Notice> findAllOrderedByIsTopAndCategory(@Param("category") NoticeCategory category, Pageable pageable);
+
 	@Query("SELECT n FROM Notice n ORDER BY n.isTop DESC, n.createdDate DESC")
 	Page<Notice> findAllOrderedByIsTopAndCreateDate(Pageable pageable);
 }
